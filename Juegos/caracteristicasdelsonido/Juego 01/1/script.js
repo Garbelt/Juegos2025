3adbe7ff65fadd4c2fc2f5ed117c83960ea2c2df
Garbelt <g.mcmlxxii@gmail.com>
2026-03-26 11:30:28 -0300

    Update script.js

--- Juegos/caracteristicasdelsonido/Juego 01/1/script.js
+++ Juegos/caracteristicasdelsonido/Juego 01/1/script.js
@@ -155,7 +155,7 @@ function updateTimerDisplay(totalSeconds) {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
   document.getElementById("reloj").textContent =
-    `Tiempo: ${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+    `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
 }
 
 // 🚫 / 🟢 Control de clics
