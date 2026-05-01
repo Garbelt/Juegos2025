988eef976fb1de9da8d551cc7ae2bf8822bffced
Garbelt <g.mcmlxxii@gmail.com>
2026-04-30 23:33:52 -0300

    Update script.js

--- Juegos/operaciones/Juego 01/1/script.js
+++ Juegos/operaciones/Juego 01/1/script.js
@@ -65,7 +65,7 @@ function crearTeclado(containerId) {
         [1,2,3],
         [4,5,6],
         [7,8,9],
-        ["",0,"borrar"]
+        [0,"borrar"]
     ];
 
     const keyboard = document.createElement("div");
