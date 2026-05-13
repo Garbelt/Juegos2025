6f431a90d8f38f97bba92c609cbfe7f20856632e
Garbelt <g.mcmlxxii@gmail.com>
2026-05-13 01:18:37 -0300

    Update script.js

--- Juegos/vocales/Juego 02/1/script.js
+++ Juegos/vocales/Juego 02/1/script.js
@@ -373,7 +373,7 @@ function showMessagexExito() {
         usuario: localStorage.getItem("ActualUs"),
         puntaje: score,
         juegonumero: incrementGameNumber(),
-        game: "AEI_sdl",
+        game: "VCLS_sdl1",
         rutina: localStorage.getItem("rutina")
       };
 
